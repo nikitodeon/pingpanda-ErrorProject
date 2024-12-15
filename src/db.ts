3fedd9d75dfa461a4c39ac224a422699f4f22dd7
Nikita <ragerage333@gmail.com>
2024-12-15 10:16:41 +0300

    Initial commit

--- src/db.ts
+++ src/db.ts
@@ -1,24 +1,31 @@
-import { Pool } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
+// import { Pool } from "@neondatabase/serverless"
+// import { PrismaNeon } from "@prisma/adapter-neon"
 import { PrismaClient } from "@prisma/client"
 
 declare global {
   // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient
+  var cachedPrisma: PrismaClient | undefined
 }
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === "production") {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-  const adapter = new PrismaNeon(pool)
-  prisma = new PrismaClient({ adapter })
-} else {
-  if (!global.cachedPrisma) {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-    const adapter = new PrismaNeon(pool)
-    global.cachedPrisma = new PrismaClient({ adapter })
-  }
-  prisma = global.cachedPrisma
-}
+// let prisma: PrismaClient
+// if (process.env.NODE_ENV === "production") {
+//   const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+//   const adapter = new PrismaNeon(pool)
+//   prisma = new PrismaClient({ adapter })
+// } else {
+//   if (!global.cachedPrisma) {
+//     const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+//     const adapter = new PrismaNeon(pool)
+//     global.cachedPrisma = new PrismaClient({ adapter })
+//   }
+//   prisma = global.cachedPrisma
+// }
 
+// export const db = prisma
+
+const prisma = global.cachedPrisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") {
+  global.cachedPrisma = prisma
+}
 export const db = prisma
--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,43 +1,34 @@
-import { Pool } from "@neondatabase/serverless"
-import { PrismaNeon } from "@prisma/adapter-neon"
+import { j } from "./__internals/j"
+// import { PrismaNeon } from "@prisma/adapter-neon"
+// import { Pool } from "@neondatabase/serverless"
 import { PrismaClient } from "@prisma/client"
 import { Redis } from "@upstash/redis/cloudflare"
 import { env } from "hono/adapter"
 import { cacheExtension } from "./__internals/db/cache-extension"
-import { j } from "./__internals/j"
-
-/**
- * Middleware for providing a built-in cache with your Prisma database.
- *
- * You can remove this if you don't like it, but caching can massively speed up your database queries.
- */
-
-const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
-  const variables = env(c)
-
-  const pool = new Pool({
-    connectionString: variables.DATABASE_URL,
-  })
-
-  const adapter = new PrismaNeon(pool)
-
-  const redis = new Redis({
-    token: variables.REDIS_TOKEN,
-    url: variables.REDIS_URL,
-  })
-
-  const db = new PrismaClient({
-    adapter,
-  }).$extends(cacheExtension({ redis }))
-
-  // Whatever you put inside of `next` is accessible to all following middlewares
-  return await next({ db })
-})
-
 /**
  * Public (unauthenticated) procedures
  *
  * This is the base piece you use to build new queries and mutations on your API.
  */
+// const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
+//   const variables = env(c)
+//   const pool = new Pool({
+//     connectionString: variables.DATABASE_URL,
+//   })
+//   // const adapter = new PrismaNeon(pool)
+
+//   const db = new PrismaClient({
+//     // adapter,
+//   })
+//   return await next({ db })
+// })
+
+const authMiddleware = j.middleware(({ next }) => {
+  const user = { name: "John" }
+  return next({ user })
+})
+
 export const baseProcedure = j.procedure
-export const publicProcedure = baseProcedure.use(extendedDatabaseMiddleware)
+export const publicProcedure =
+  baseProcedure /*.use(extendedDatabaseMiddleware)*/
+export const privateProcedure = publicProcedure.use(authMiddleware)
